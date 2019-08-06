7e8d3bb63e4303dd6f6525de32837086113b4627
Jose Manuel Delicado <jmdaweb@hotmail.com>
2019-08-06 12:20:43 +0200

    Updated add-on development guide. Updated genpot to use xgettext rather than Python

--- scripts/genpot.bat
+++ scripts/genpot.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Generating application translation strings...
 for %%i in (..\python_docs\*.py) do (
-python pygettext.py -v -d ..\potfiles\%%~ni ..\python_docs\%%~ni.py
+xgettext -o ..\potfiles\%%~ni.pot ..\python_docs\%%~ni.py
 )
