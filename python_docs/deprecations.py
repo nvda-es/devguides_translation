42b0d487300db55ff972fd23e2d95c4c47427d7a
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2021-07-23 19:06:05 +0200

    Updated deprecations and add-on store proposal

--- python_docs/deprecations.py
+++ python_docs/deprecations.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 documentation = [
-_(u"""Occasionally changes have to be made to NVDA that will break compatibility with prior versions of the public API. In order to make this predictable for add-on authors, NV Access has adopted a schedule. The first release of each year (starting with 2021.1) will be the opportunity to remove deprecated code, or make other compatibility breaking design changes. Other releases will aim to maintain compatibility, sometimes keeping code but marking it as deprecated. When marking code as deprecated, the definition should have a comment to explain such and the PR that introduces the change will be tagged with the `deprecated/20YY.1`. Where `YY` is the release year."""),
+_(u"""The NVDA API must maintain compatibility with add-ons throughout yearly development cycles."""),
+_(u"""The first release of a year, i.e. `20XX.1`, is when the NVDA API can introduce breaking changes."""),
+"",_(u"""In order to improve the NVDA API, changes that will break future compatibility can be implemented, as long they retain backwards compatibility until the `20XX.1` release."""),
+"",_(u"""This can be done by using a version check to automate deprecation. For example, if you wish to replace usages of `foo` with `bar`. When we begin work on `NEXT_YEAR`, `foo` will no longer be part of the NVDA API and all internal usages must be removed prior. """),
+_(u"""```python"""),
+_(u"""from buildVersion import version_year"""),
+_(u"""if version_year < NEXT_YEAR:"""),
+_(u"""	foo = bar"""),
+_(u"""```"""),
+"",_(u"""To ensure a module retains the same symbol names being importable, check across versions what is imported using the NVDA python console."""),
+_(u"""```python"""),
+_(u"""import controlTypes"""),
+_(u"""dir(controlTypes)"""),
+_(u"""```"""),
+"",_(u"""Changes different to moving or renaming symbols need to be considered carefully with a different approach. """),
+"",_(u"""Any API breaking changes such as deprecations marked for removal should be commented with the year of intended removal, and notes on how to implement the API change as an add-on developer and NVDA developer."""),
 ]
